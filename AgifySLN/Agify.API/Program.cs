a1a6221a70154e072c2b643f6d0a496c937b808d
BarannUnsal <barannunsal@gmail.com>
2023-01-08 18:22:58 +0300

    docker compose.yml create and mssql configuration

--- AgifySLN/Agify.API/Program.cs
+++ AgifySLN/Agify.API/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
 builder.Services.AddBLSerivce();
 builder.Services.AddDALService();
