348580b07a2c019578dd2a39c6f88a2ebef0ea51
Baran <96000792+BarannUnsal@users.noreply.github.com>
2023-01-13 18:24:00 +0300

    return object

--- AgifySLN/Agify.DAL/Abstract/IUserRepository.cs
+++ AgifySLN/Agify.DAL/Abstract/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace Agify.DAL.Abstract
 {
     public interface IUserRepository
     {
-        Task<User> GetAsync(string name);
+        Task<object> GetAsync(string name);
         Task<User[]> GetArrayAsync(string[] names);
         Task<bool> AddUserAsync(User user);
         Task<bool> AddUserRangeAsync(User[] users);
