85f5da56ed44e0ed598882024e9bc1f7ff4d08e9
Baran <96000792+BarannUnsal@users.noreply.github.com>
2023-01-13 18:18:02 +0300

    user age and count int to string

--- AgifySLN/Agify.Domain/Entities/User.cs
+++ AgifySLN/Agify.Domain/Entities/User.cs
@@ -1,40 +1,39 @@
-﻿using Agify.Domain.Enums;
-using System.ComponentModel.DataAnnotations;
+using Agify.Domain.Enums;
 
 namespace Agify.Domain.Entities
 {
     public class User
     {
         public int Id{ get; set; }
-        public int? Age { get; set; }
+        public string? Age { get; set; }
         public string? Name { get; set; }
-        public int? Count { get; set; }
+        public string? Count { get; set; }
 
         public string? StageOfLife
         {
             get
             {
-                if (Age >= 0 && Age < 7)
+                if (int.Parse(Age) >= 0 && int.Parse(Age) < 7)
                 {
                     return ((StageOfLife)1).ToString();
                 }
-                else if (Age >= 7 && Age < 14)
+                else if (int.Parse(Age) >= 7 && int.Parse(Age) < 14)
                 {
                     return ((StageOfLife)2).ToString();
                 }
-                else if (Age >= 14 && Age < 28)
+                else if (int.Parse(Age) >= 14 && int.Parse(Age) < 28)
                 {
                     return ((StageOfLife)3).ToString();
                 }
-                else if (Age >= 28 && Age < 50)
+                else if (int.Parse(Age) >= 28 && int.Parse(Age) < 50)
                 {
                     return ((StageOfLife)4).ToString();
                 }
-                else if (Age >= 50 && Age < 70)
+                else if (int.Parse(Age) >= 50 && int.Parse(Age) < 70)
                 {
                     return ((StageOfLife)5).ToString();
                 }
-                else if (Age >= 70)
+                else if (int.Parse(Age) >= 70)
                 {
                     return ((StageOfLife)6).ToString();
                 }
